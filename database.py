ca747804d8ba68f71dcf5a78600d567f980eed29
Reverb C <reverbc@me.com>
2017-10-07 20:34:47 -0700

    do not import model in database

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ def init_db(app):
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    import models as _
+    # import models as _
 
     global DB_SESSION  # pylint: disable=global-statement
 
